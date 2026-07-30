9d3ee37e1c744c566ef6bab7ebf6c2d58bb05a0c
aescobar-applaudo <aescobar@applaudostudios.com>
2026-07-29 19:17:21 -0600

    refactor DeunaPaymentDialog to initialize SDK with order and user tokens; enhance callback handling

--- app/src/main/kotlin/ui/dialog/DeunaPaymentDialog.kt
+++ app/src/main/kotlin/ui/dialog/DeunaPaymentDialog.kt
@@ -2,11 +2,11 @@ package ui.dialog
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.webkit.WebView
 import androidx.fragment.app.DialogFragment
 import com.deuna.maven.DeunaSDK
-import com.deuna.maven.ElementsWidgetExperience
 import com.deuna.maven.generateFraudId
 import com.deuna.maven.initElements
 import com.deuna.maven.shared.ElementsCallbacks
@@ -14,6 +14,7 @@ import com.deuna.maven.shared.Environment
 import com.deuna.maven.shared.domain.UserInfo
 import com.example.deuna_integration.BuildConfig
 import com.example.deuna_integration.R
+
 
 class DeunaPaymentDialog: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -31,68 +32,58 @@ class DeunaPaymentDialog: DialogFragment() {
             publicApiKey = BuildConfig.DEUNA_PUBLIC_API_KEY
         )
 
-        var fraudId: String? = null
+                deunaSDK.initElements(
+                    context = requireContext(),
+                    orderToken = BuildConfig.DEUNA_ORDER_TOKEN, //call /api/v1/deuna/order
+                    //required if user is registered
+                    userToken = BuildConfig.DEUNA_USER_TOKEN, //call /api/v1/deuna/session
 
-        deunaSDK.generateFraudId(
-            context = requireContext(),
-            params = mapOf(
-                "ACCERTIFY" to mapOf(
-                "src" to "sr",
-                "id" to "id",
-                "dvc" to "dvc",
-                "collectorHost" to "collectorHost",
-                "sid" to "sid"
-                ),
-                "RISKIFIED" to mapOf(
-                    "storeDomain" to ""
-                )
-            ),
-            callback = { fraudIdResult ->
-                Log.e("DeunaPaymentDialog", "Fraud ID: $fraudIdResult")
-                fraudId = fraudIdResult
-            }
-        )
+                    //Required if userToken is not provided / anonymous user
+                  userInfo = UserInfo(
+                      firstName = "John",
+                      lastName = "Test",
+                      email = "jdoe2026@mail.com"
+                  ),
+                    callbacks = ElementsCallbacks().apply {
+                        onSuccess = { data ->
+                            Log.d("DeunaPaymentDialog", "onSuccess data: $data")
 
-        deunaSDK.initElements(
-            context = requireContext(),
-            orderToken = "your-order-token-here", //call /api/v1/deuna/order
-            //required if user is registered
-            //userToken = "user-token-here", //call /api/v1/deuna/session
+                            deunaSDK.close() // Close the DialogFragment of the payment widget
+                            // Your additional code
 
-            //Required if userToken is not provided / anonymous user
-            userInfo = UserInfo(
-                firstName= "John",
-                lastName= "Doe",
-                email= "j.doe@mail.com"),
+                        }
+                        onError = { error ->
+                            // Error handling
+                            Log.e("DeunaPaymentDialog", "onError: $error")
 
-            callbacks = ElementsCallbacks().apply {
-                onSuccess = { data ->
-                    Log.d("DeunaPaymentDialog", "onSuccess data: $data")
-                    Log.d("DeunaPaymentDialog", "Fraud ID used: $fraudId")
-                    deunaSDK.close() // Close the DialogFragment of the payment widget
-                    // Your additional code
-                }
+                        }
+                        onClosed = { action ->
+                            // Widget closed
+                        }
+                        onEventDispatch = { event, data ->
+                            Log.d("DeunaPaymentDialog", "Event: $event data: $data")
+                        }
+                    },
+    //            widgetExperience = ElementsWidgetExperience( // optional
+    //                userExperience = ElementsWidgetExperience.UserExperience(
+    //                    showSavedCardFlow = false, // optional
+    //                    defaultCardFlow = true // optional
+    //                )
+    //            ),
+                    domain= BuildConfig.DEUNA_DOMAIN,
+                    language = "en",
 
-                onError = { error ->
-                    // Error handling
-                    Log.e("DeunaPaymentDialog", "onError: $error")
-
-                }
-                onClosed = { action ->
-                    // Widget closed
-                }
-            },
-            widgetExperience = ElementsWidgetExperience( // optional
-                userExperience = ElementsWidgetExperience.UserExperience(
-                    showSavedCardFlow = false, // optional
-                    defaultCardFlow = true // optional
-                )
-            ),
-            domain= BuildConfig.DEUNA_DOMAIN,
-            language = "en",
+                    )
 
 
-        )
+
+
         return dialog
     }
+
+
+fun decodeBase64(encoded: String): String {
+    val decodedBytes = Base64.decode(encoded, Base64.DEFAULT)
+    return String(decodedBytes, Charsets.UTF_8)
+}
 }
